6f46d80d6174bf09c99e6551e0a7cf40441c38e6
Amarish Barot <amarishbarot299@gmail.com>
2022-06-24 16:53:53 +0530

    initial commit

---
+++ dict.py
@@ -0,0 +1,69 @@
+# dic1={"name":"ajay","age":25,"salary":25000,"city":"mahuva"}
+# for key,value in dic1.items():
+#     print(key,value)
+
+
+# no=int(input("enter how many keys and values add to d1:"))
+# d1={}
+# for i in range(no):
+#     k=input("enter the key:")
+#     v=input("enter the value:")
+#     d1[k]=v
+    
+#print(d1)
+
+
+# dts1={}
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# for x in thisdict.values():
+#   print(x)
+# # #print(thisdict['year'])
+# # print(thisdict.values()
+      
+# dts1={}
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# thisdict.update({"color":"red"})
+# #dts1.update(thisdict)
+# dts1=dict(thisdict)
+# print(dts1)
+
+#from multiprocessing.sharedctypes import Value
+
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+
+# dts1 = {}
+# for key in thisdict:
+#   dts1[key] = thisdict[key]
+# # dts1[Value] = thisdict[Value]
+  
+
+# print(thisdict) 
+# print(dts1) 
+# dts1["color"] = "red"
+# print(thisdict) 
+# print(dts1)
+
+
+# mydict={}
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# mydict = dict(thisdict)
+# print(mydict)
+# mydict=thisdict.update({"color":"red"})
+# print(thisdict)
